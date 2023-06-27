6b752148d15b410c7a691698ac78a986b3f3b179
Matthew Bauer <bauermatt18@gmail.com>
2023-06-26 23:36:46 -0700

    updated gardens script to convert kmls to shp

--- Community_Gardens_Parks.py
+++ Community_Gardens_Parks.py
@@ -1,8 +1,11 @@
 import requests
 import subprocess
-
-# specify the directory
-directory = 'C:/Users/bauer/Google Drive/USC/Work/Research/DataPipeline/'
+import pandas as pd
+import geopandas as gpd
+from shapely.ops import unary_union
+from geopandas.tools import sjoin
+import os
+import shutil
 
 # Parks and gardens URL
 parks_gardens_url = "https://www.google.com/maps/d/u/0/kml?mid=1gAIltWAx3egz2rjqB4cd3bL6I0yWOwM9&forcekml=1"
@@ -11,7 +14,7 @@ parks_gardens_url = "https://www.google.com/maps/d/u/0/kml?mid=1gAIltWAx3egz2rjq
 parks_gardens_response = requests.get(parks_gardens_url)
 
 # Save the file
-with open(directory + 'parksandgardens.kml', 'wb') as f:
+with open('parksandgardens.kml', 'wb') as f:
     f.write(parks_gardens_response.content)
 
 # Gardens URL
@@ -21,12 +24,12 @@ gardens_url = "https://www.google.com/maps/d/u/0/kml?mid=1HXMaJgriQuBFj8_qxpXZLn
 gardens_response = requests.get(gardens_url)
 
 # Save the file
-with open(directory + 'gardens.kml', 'wb') as f:
+with open('gardens.kml', 'wb') as f:
     f.write(gardens_response.content)
 
 # paths to input KML and output shapefile
-parks_gardens_kml = directory + "parksandgardens.kml"
-parks_gardens_shapefile = directory + "parksandgardens.shp"
+parks_gardens_kml = "parksandgardens.kml"
+parks_gardens_shapefile = "parksandgardens.shp"
 
 # construct the ogr2ogr command
 parks_gardens_command = f'ogr2ogr -f "ESRI Shapefile" {parks_gardens_shapefile} {parks_gardens_kml}'
@@ -35,8 +38,8 @@ parks_gardens_command = f'ogr2ogr -f "ESRI Shapefile" {parks_gardens_shapefile} 
 subprocess.run(parks_gardens_command, shell=True)
 
 # paths to input KML and output shapefile
-gardens_kml = directory + "gardens.kml"
-gardens_shapefile = directory + "gardens.shp"
+gardens_kml = "gardens.kml"
+gardens_shapefile = "gardens.shp"
 
 # construct the ogr2ogr command
 gardens_command = f'ogr2ogr -f "ESRI Shapefile" {gardens_shapefile} {gardens_kml}'
@@ -48,14 +51,62 @@ subprocess.run(gardens_command, shell=True)
 parks_gardens_result = subprocess.run(
     parks_gardens_command, shell=True, capture_output=True, text=True)
 
-# print output and errors
-print("Parks and Gardens stdout:\n", parks_gardens_result.stdout)
-print("Parks and Gardens stderr:\n", parks_gardens_result.stderr)
+# Load the shapefiles into GeoDataFrames
+both_gdf = gpd.read_file("parksandgardens.shp/both.shp")
+gardens_gdf = gpd.read_file("parksandgardens.shp/gardens.shp")
+parks_gdf = gpd.read_file("parksandgardens.shp/parks.shp")
+communitygardens_gdf = gpd.read_file(
+    "gardens.shp/Community Gardens in L.A. County.shp")
+LACGC_gdf = gpd.read_file("gardens.shp/LACGC Community Gardens.shp")
 
-# execute the command and capture output and errors
-gardens_result = subprocess.run(
-    gardens_command, shell=True, capture_output=True, text=True)
+# Concatenate the geodataframes
+combined_gdf = pd.concat([both_gdf, gardens_gdf, parks_gdf,
+                         communitygardens_gdf, LACGC_gdf], ignore_index=True)
 
-# print output and errors
-print("Gardens stdout:\n", gardens_result.stdout)
-print("Gardens stderr:\n", gardens_result.stderr)
+# Remove duplicates based on the 'Name' field and geometry
+combined_gdf = combined_gdf.drop_duplicates(subset=['Name', 'geometry'])
+
+# Save the original CRS
+original_crs = combined_gdf.crs
+
+# Remove duplicates based on the 'Name' field and geometry
+combined_gdf = combined_gdf.drop_duplicates(subset=['Name', 'geometry'])
+
+# Project the data to UTM zone 11N (appropriate for Los Angeles)
+combined_gdf = combined_gdf.to_crs("EPSG:26911")
+
+# Create a buffer of 10 feet around each point
+buffer = combined_gdf.buffer(20)
+
+# Dissolve all geometries into one
+all_areas = unary_union(buffer)
+
+# Convert the unary_union result to a list of geometries
+all_areas = [geom for geom in all_areas]
+
+# Create a new GeoDataFrame from the list of geometries, setting the original CRS
+buffer_gdf = gpd.GeoDataFrame(gpd.GeoSeries(all_areas), columns=[
+                              'geometry'], crs="EPSG:26911")
+
+# Create unique IDs for the new GeoDataFrame
+buffer_gdf['ID'] = range(len(buffer_gdf))
+
+# Spatially join the buffers with the original GeoDataFrame
+joined = gpd.sjoin(combined_gdf, buffer_gdf, how='left', op='within')
+
+# Keep only the first point in each group of points within each buffer area
+unique_points = joined.sort_values('ID').groupby('ID').first()
+
+# Set the CRS for the 'unique_points' DataFrame
+unique_points.set_crs("EPSG:26911", inplace=True)
+
+# Reproject back to the original CRS
+unique_points = unique_points.to_crs(original_crs)
+
+# Save the result as a new shapefile
+unique_points.to_file("unique_points.shp")
+
+shutil.rmtree("gardens.shp")
+shutil.rmtree("parksandgardens.shp")
+os.remove("gardens.kml")
+os.remove("parksandgardens.kml")
