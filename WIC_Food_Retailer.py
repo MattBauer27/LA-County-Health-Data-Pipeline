55f3ad4334bcd4ddfda84dab65ef584a90a32946
Matthew Bauer <bauermatt18@gmail.com>
2023-07-03 00:37:09 -0700

    improved records for wic food retailer script

--- WIC_Food_Retailer.py
+++ WIC_Food_Retailer.py
@@ -1,249 +1,226 @@
+from arcgis.features import FeatureLayer
+from arcgis.gis import GIS
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+import requests
+import pandas as pd
+from arcgis.geocoding import geocode
+import geopandas as gpd
+from shapely.geometry import Point
+from arcgis.features import FeatureLayerCollection
+import logging
+import datetime
 import json
 import urllib.request
-import pandas as pd
-import os
 import requests
-import camelot
-from arcgis.gis import GIS
-from arcgis.geocoding import geocode
-from dotenv import load_dotenv
-from arcgis.features import FeatureLayerCollection
+from xml.etree import ElementTree as ET
 
 load_dotenv()
 
-# Initialize the GIS
-gis = GIS("https://uscssi.maps.arcgis.com", client_id=os.getenv('client_id'))
-
-# Define your username and folder
-username = "mkbauer_USCSSI"
-folder_name = os.getenv("folder_name")
-
-url = 'https://data.chhs.ca.gov/api/3/action/datastore_search?resource_id=ee10b67b-2b93-47e7-aa41-cecfbbd32e17&limit=100'
-offset = 0
-records = []
-
-while True:
-    response = urllib.request.urlopen(f"{url}&offset={offset}")
-    response_dict = json.loads(response.read())
-    records_batch = response_dict['result']['records']
-
-    # If no more records, break the loop
-    if not records_batch:
-        break
-
-    records.extend(records_batch)
-    offset += len(records_batch)
-
-# Load the records into a DataFrame
-df1 = pd.DataFrame(records)
-
-# Clean all whitespace and newlines from all columns
-df1 = df1.applymap(lambda x: x.strip().replace(
-    '\n', ' ') if isinstance(x, str) else x)
-
-""" # Limit the number of rows in df1 for testing
-df1 = df1.head(500) """
-
-# Check if the necessary columns exist in df1
-assert '_id' in df1.columns, "Column '_id' not found in df1"
-assert 'ADDRESS' in df1.columns, "Column 'ADDRESS' not found in df1"
-assert 'VENDOR' in df1.columns, "Column 'VENDOR' not found in df1"
-
-# Drop the unnecessary columns
-df1 = df1.drop(columns=['_id'])
-
-df1 = df1.dropna(subset=['ADDRESS'])
-
-# Export the DataFrame to a CSV file
-df1.to_csv('records.csv', index=False, encoding='utf-8-sig')
-
-# URL of the PDF file
-WICCard_URL = "https://www.cdph.ca.gov/Programs/CFH/DWICSN/CDPH%20Document%20Library/WICCard/Stores%20Accepting%20California%20WIC%20Card.pdf"
-
-# Path where the PDF file will be saved temporarily
-WICCard_PDF = "temp.pdf"
-
-# Download the PDF file
-response = requests.get(WICCard_URL)
-with open(WICCard_PDF, 'wb') as f:
-    f.write(response.content)
-
-# Read the PDF as a list of Table objects
-tables = camelot.read_pdf(WICCard_PDF, pages='all')
-
-# Concatenate all the DataFrames into a single DataFrame
-df2 = pd.concat([table.df for table in tables])
-
-# Set the first row as the column names
-df2.columns = df2.iloc[0]
-
-# Remove the first row
-df2 = df2[1:]
-
-# Identify the rows that are identical to the column headers
-header_rows = df2.eq(df2.columns)
-
-# Check if all values in a row are True (i.e., the row is a header row)
-header_rows = header_rows.all(axis=1)
-
-# Keep only the rows that are not header rows
-df2 = df2[~header_rows]
-
-# Clean all whitespace and newlines from all columns
-df2 = df2.applymap(lambda x: x.strip().replace(
-    '\n', ' ') if isinstance(x, str) else x)
-
-# Replace all whitespace characters with a single space in column names
-df2.columns = df2.columns.str.replace(r'\s+', ' ', regex=True).str.strip()
-
-""" # Limit the number of rows in df2 for testing
-df2 = df2.head(500) """
-
-# Check if the necessary columns exist in df2
-assert 'STORE NAME' in df2.columns, "Column 'STORE NAME' not found in df2"
-assert 'STORE ADDRESS' in df2.columns, "Column 'STORE ADDRESS' not found in df2"
-assert 'STORE PHONE NUMBER' in df2.columns, "Column 'STORE PHONE NUMBER' not found in df2"
-
-# Export the DataFrame to a CSV file
-df2.to_csv('records2.csv', index=False, encoding='utf-8-sig')
-
-
-# Add this line before the merge operation
-df1['STORE PHONE NUMBER'] = None
-
-df1['ADDRESS'] = df1['ADDRESS'].str.strip().str.replace(r'\s+', ' ', regex=True)
-
-# Identify addresses that appear more than once in either DataFrame
-duplicate_addresses_df1 = df1['ADDRESS'].value_counts() > 1
-duplicate_addresses_df2 = df2['STORE ADDRESS'].value_counts() > 1
-
-duplicate_addresses = set(duplicate_addresses_df1.index[duplicate_addresses_df1]) | set(
-    duplicate_addresses_df2.index[duplicate_addresses_df2])
-
-# Create a mask for rows in df1 that have a duplicate address
-mask_df1 = df1['ADDRESS'].isin(duplicate_addresses)
-
-# Create a mask for rows in df2 that have a duplicate address
-mask_df2 = df2['STORE ADDRESS'].isin(duplicate_addresses)
-
-# Add this line before the merge operation
-df1['STORE PHONE NUMBER'] = None
-
-# Identify rows in df1 and df2 that have a duplicate address
-mask_df1 = df1['ADDRESS'].duplicated(keep=False)
-mask_df2 = df2['STORE ADDRESS'].duplicated(keep=False)
-
-# Merge on 'ADDRESS' and 'STORE ADDRESS' only for rows that do not have a duplicate address
-merged_df1 = df1[~mask_df1].merge(df2[~mask_df2][['STORE ADDRESS', 'STORE PHONE NUMBER']],
-                                  left_on='ADDRESS', right_on='STORE ADDRESS', how='left').drop(columns=['STORE PHONE NUMBER_x'])
-
-# For rows that have a duplicate address, merge on 'ADDRESS', 'STORE ADDRESS', 'VENDOR', and 'STORE NAME'
-merged_df2 = df1[mask_df1].merge(df2[mask_df2][['STORE ADDRESS', 'STORE NAME', 'STORE PHONE NUMBER']], left_on=[
-                                 'ADDRESS', 'VENDOR'], right_on=['STORE ADDRESS', 'STORE NAME'], how='left').drop(columns=['STORE PHONE NUMBER_x'])
-
-# Concatenate the two DataFrames
-merged_df = pd.concat([merged_df1, merged_df2])
-
-# Reset the index
-merged_df.reset_index(drop=True, inplace=True)
-
-# Directly assign the 'STORE PHONE NUMBER' column in df1 with the 'STORE PHONE NUMBER_y' column in merged_df
-df1['STORE PHONE NUMBER'] = merged_df['STORE PHONE NUMBER_y']
-
-# Convert the 'ZIP' column to integers
-df1['ZIP'] = df1['ZIP'].astype(int)
-
-# Concatenate 'ADDRESS', 'SECOND ADDRESS', 'CITY', and 'ZIP' into a single string
-df1['ADDRESS'] = df1['ADDRESS'] + ', ' + df1['SECOND ADDRESS'] + \
-    ', ' + df1['CITY'] + ', ' + df1['ZIP'].astype(str)
-
-# Convert the string to title case
-df1['ADDRESS'] = df1['ADDRESS'].str.title()
-
-# Convert the string to title case
-df1['VENDOR'] = df1['VENDOR'].str.title()
-
-df1 = df1.drop(columns=['CITY', 'SECOND ADDRESS', 'ZIP'])
-
-# Reorder the columns
-df1 = df1[['VENDOR', 'ADDRESS', 'COUNTY',
-           'STORE PHONE NUMBER', 'LATITUDE', 'LONGITUDE']]
-
-# Rename the columns and convert to title case
-df1 = df1.rename(columns={
-    'VENDOR': 'Name',
-    'ADDRESS': 'Address',
-    'COUNTY': 'County',
-    'STORE PHONE NUMBER': 'Phone Number',
-    'LATITUDE': 'Latitude',
-    'LONGITUDE': 'Longitude'
-})
-
-# Export the DataFrame to a CSV file
-df1.to_csv('records3.csv', index=False, encoding='utf-8-sig')
-
-for i, row in df1[df1['Latitude'].isnull() | df1['Longitude'].isnull()].iterrows():
-    try:
-        geocoded = geocode(row['Address'])[0]
-        df1.at[i, 'Latitude'] = geocoded['location']['y']
-        df1.at[i, 'Longitude'] = geocoded['location']['x']
-    except IndexError:
-        print(f"Could not geocode address: {row['Address']}")
-
-# Define the name of the feature layer
-feature_layer_name = 'WIC Food Retailer'
-
-# Search for an existing feature layer with the same name
-search_result = gis.content.search(
-    query=f'title:{feature_layer_name} AND owner:{username}', item_type='Feature Layer')
-
-# Export the DataFrame to a CSV file
-csv_file = 'records_final.csv'
-df1.to_csv(csv_file, index=False)
-
-# Define the name of the CSV item
-csv_item_name = 'records_final'
-
-# Search for an existing CSV item with the same name
-search_result = gis.content.search(
-    query=f'title:{csv_item_name} AND owner:{username}', item_type='CSV')
-
-# If the CSV item exists, delete it
-if search_result:
-    for item in search_result:
-        item.delete()
-
-# If the feature layer exists, overwrite it
-if search_result:
-    # Assume the first search result is the correct item
-    feature_layer_item = search_result[0]
-
-    # Upload the CSV file to ArcGIS Online with the specified title
-    csv_item = gis.content.add({'title': feature_layer_name}, csv_file)
-
-    # Publish the CSV file as a hosted feature layer
-    new_feature_layer_item = csv_item.publish()
-
-    # Create a FeatureLayerCollection from the item
-    feature_layer_collection = FeatureLayerCollection.fromitem(
-        feature_layer_item)
-
-    # Overwrite the feature layer with the new feature layer
-    feature_layer_collection.manager.update(
-        {'url': new_feature_layer_item.url})
-
-    # Delete the new feature layer item and the CSV item
-    new_feature_layer_item.delete()
-    csv_item.delete()
-else:
-    # Upload the CSV file to ArcGIS Online with the specified title
-    csv_item = gis.content.add({'title': feature_layer_name}, csv_file)
-
-    # Publish the CSV file as a hosted feature layer
-    feature_layer_item = csv_item.publish()
-
-    # Move the item to a folder
-    feature_layer_item.move(folder=folder_name)
-
-    # Delete the CSV item
-    csv_item.delete()
+# create logger with 'spam_application'
+logger = logging.getLogger('run_log')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+now = datetime.datetime.now()
+logging_date = now.strftime("%Y-%m-%d")
+fh = logging.FileHandler(f'WIC_Food_Retailers/RunLog/run_log.csv')
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+try:
+
+    # Initialize the GIS
+    gis_insp = GIS("https://uscssi.maps.arcgis.com",
+                   client_id=os.getenv('client_id'))
+
+    # Define your username and folder
+    username = "mkbauer_USCSSI"
+    folder_name = os.getenv("folder_name")
+
+    url = 'https://data.chhs.ca.gov/datastore/odata3.0/ee10b67b-2b93-47e7-aa41-cecfbbd32e17?$top=1&$format=xml'
+    ns = {'default': 'http://www.w3.org/2005/Atom'}  # define namespace
+
+    response = urllib.request.urlopen(url)
+    data = response.read()  # a `bytes` object
+    xml_content = data.decode('utf-8')
+
+    root = ET.fromstring(xml_content)
+    updated_element = root.find('default:updated', ns)
+    if updated_element is not None:
+        # remove the last character 'Z'
+        updated_text = updated_element.text[:-1]
+        updated_datetime = datetime.datetime.strptime(
+            updated_text, "%Y-%m-%dT%H:%M:%S.%f")
+        updated_date = updated_datetime.date()  # Convert to date
+        updated_date = updated_date.strftime("%Y-%m-%d")
+        print(updated_date)
+    else:
+        print('updated field not found')
+
+    # Check if file exists and get the last update date
+    file_path = 'WIC_Food_Retailers/Dates_txts/last_update.txt'
+    if os.path.isfile(file_path):
+        with open(file_path, 'r') as f:
+            last_recorded_dates = [line.strip()
+                                   for line in f.readlines()]  # get all lines
+    else:
+        last_recorded_dates = []
+
+    # If the last update date is more recent than the most recent date in the file, prepend it to the file
+    if not last_recorded_dates or updated_date > last_recorded_dates[0]:
+        with open(file_path, 'w') as f:
+            f.write(updated_date + '\n')
+            for recorded_date in last_recorded_dates:
+                f.write(recorded_date + '\n')
+
+        url = 'https://data.chhs.ca.gov/api/3/action/datastore_search?q=Los_Angeles&resource_id=ee10b67b-2b93-47e7-aa41-cecfbbd32e17&limit=100'
+        offset = 0
+        records = []
+
+        while True:
+            response = urllib.request.urlopen(f"{url}&offset={offset}")
+            response_dict = json.loads(response.read())
+            records_batch = response_dict['result']['records']
+
+            # If no more records, break the loop
+            if not records_batch:
+                break
+
+            records.extend(records_batch)
+            offset += len(records_batch)
+
+        # Load the records into a DataFrame
+        df1 = pd.DataFrame(records)
+
+        # Drop the unnecessary columns
+        df1 = df1.drop(columns=['_id'])
+
+        # Convert records to a pandas DataFrame
+        new_records_df = pd.DataFrame(records_insp)
+        # new_records_df = new_records_df.fillna('None')
+
+        new_records_df.to_csv(
+            f"WIC_Food_Retailers/Base_csvs/wic_food_retailers_{updated_date}.csv", index=False, encoding='utf-8')
+        if last_recorded_dates:
+            old_records_df = pd.read_csv(
+                f"WIC_Food_Retailers/Base_csvs/wic_food_retailers_{last_recorded_dates[0]}.csv", encoding='utf-8')
+
+            # old_records_df = old_records_df.fillna('None')
+
+            # Convert all columns to string type
+            old_records_df = old_records_df.astype(str)
+            new_records_df = new_records_df.astype(str)
+
+            # Dataframe for rows present in new_records_df but not in old_records_df
+            df_added = new_records_df[~new_records_df.apply(
+                tuple, 1).isin(old_records_df.apply(tuple, 1))]
+            df_added.to_csv(
+                f"WIC_Food_Retailers/Added/wic_food_retailers_{updated_date}.csv", index=False, encoding='utf-8')
+
+            # Dataframe for rows present in old_records_df but not in new_records_df
+            df_dropped = old_records_df[~old_records_df.apply(
+                tuple, 1).isin(new_records_df.apply(tuple, 1))]
+            df_dropped.to_csv(
+                f"WIC_Food_Retailers/Dropped/wic_food_retailers_{updated_date}.csv", index=False, encoding='utf-8')
+
+        logger.info("Data Updated")
+
+        # Compute the number of newly opened and closed facilities
+        opened_df = new_records_df[~new_records_df['VENDOR'].isin(
+            old_records_df['VENDOR'])]
+        opened = len(opened_df)
+
+        closed_df = old_records_df[~old_records_df['VENDOR'].isin(
+            new_records_df['VENDOR'])]
+        closed = len(closed_df)
+
+        # Create new DataFrame to store the result
+        result_df = pd.DataFrame(
+            {"Date": [update_date], "Opened": [opened], "Closed": [closed]})
+
+        # Define CSV file
+        csv_file = "WIC_Food_Retailers/Status_update/Status_updates.csv"
+
+        # If the CSV exists, load it and append the new data
+        if os.path.isfile(csv_file):
+            df = pd.read_csv(csv_file)
+            df = df.append(result_df)
+        else:
+            df = result_df
+
+        # Save to CSV
+        df.to_csv(csv_file, index=False)
+
+        # Find the feature layer to update
+        feature_layer_name = "WIC_Food_Retailers"
+        search_result = gis_insp.content.search(
+            query=f"title:\"{feature_layer_name}\" AND owner:{username}", item_type="Feature Service")
+
+        location_dict = {}
+        # Load the existing GeoJSON file
+        if last_recorded_dates:
+            previous_geojson_file = f"WIC_Food_Retailers/Geocoded_geojson/wic_food_retailers_{last_recorded_dates[0]}.geojson"
+            if os.path.isfile(previous_geojson_file):
+                previous_gdf = gpd.read_file(previous_geojson_file)
+
+                # Create a dictionary where keys are composite keys (FACILITY_ID, FACILITY_NAME, FACILITY_ADDRESS) and
+                # values are (LATITUDE, LONGITUDE) tuples
+                location_dict = {f"{row['VENDOR']}_{row['ADDRESS']}": (row['geometry'].y, row['geometry'].x)
+                                 for _, row in previous_gdf.iterrows()}
+
+        # Transform DataFrame to GeoDataFrame
+        geometry = [Point(xy) for xy in zip(
+            df.LONGITUDE, df.LATITUDE)]
+        df = df.drop(['LONGITUDE', 'LATITUDE'], axis=1)
+        gdf = gpd.GeoDataFrame(df, geometry=geometry)
+
+        # Save GeoDataFrame to GeoJSON
+        gdf.to_file(
+            f"WIC_Food_Retailers/Geocoded_geojson/wic_food_retailers_{update_date}.geojson", driver='GeoJSON')
+
+        # Define the GeoJSON file path
+        geojson_file = f"WIC_Food_Retailers/Geocoded_geojson/wic_food_retailers_{update_date}.geojson"
+
+        # If service already exists, overwrite its data
+        if search_result:
+            feature_layer_item = search_result[0]
+
+            # Create a FeatureLayerCollection from the item
+            feature_layer_collection = FeatureLayerCollection.fromitem(
+                feature_layer_item)
+
+            # Overwrite the feature layer using the GeoJSON file
+            feature_layer_collection.manager.overwrite(geojson_file)
+        else:
+            # If service does not exist publish the GeoJSON file as a new feature layer
+            item_properties = {
+                "title": feature_layer_name,
+                "type": "GeoJson",
+                "tags": ["ArcGIS Python API"],
+                "description": "Description about the GeoJson File",
+            }
+            geojson_item = gis_insp.content.add(
+                item_properties, geojson_file)
+            feature_layer_item = geojson_item.publish()
+
+        # Move the item to a folder
+        feature_layer_item.move(folder=folder_name)
+
+        print("Finished updating: {} – ID: {}".format(
+            feature_layer_item.title, feature_layer_item.id))
+
+    else:
+        logger.info("No Update")
+
+except Exception as e:
+    logger.error("Exception occurred", exc_info=True)
